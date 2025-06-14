fe9aa1062abd1fefb90a17e6d3d0b6b652281ce6
swrnz <server.weronz.manage@gmail.com>
2025-06-14 12:37:30 +0530

    update viye config

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//    plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
-// })
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path'; // ✅ Add this
+import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // ✅ This makes @ point to /src
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
