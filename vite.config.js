5564e219277c3e288ecb022a13219fc7c9a9cc73
swrnz <server.weronz.manage@gmail.com>
2025-06-14 12:39:46 +0530

    update viye config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path'; // ✅ Add this
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'), // ✅ This makes @ point to /src
+//     },
+//   },
+// });
+
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        console.warn('ROLLUP WARNING:', warning);
+        warn(warning); // Keep default behavior
+      },
     },
   },
 });
